4e495fc8171ee21c3f00d07749aa1b0c5d5ef26c
RickyMambu <rickymambu14@gmail.com>
2024-10-23 17:46:42 +0800

    Login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,7 +46,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1572, 761);
+            this.pictureBox1.Size = new System.Drawing.Size(1072, 761);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -56,11 +56,11 @@
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Arial Black", 65F, System.Drawing.FontStyle.Bold);
+            this.label1.Font = new System.Drawing.Font("Arial Black", 35F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(-31, 0);
+            this.label1.Location = new System.Drawing.Point(26, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(834, 379);
+            this.label1.Size = new System.Drawing.Size(682, 305);
             this.label1.TabIndex = 1;
             this.label1.Text = "WELCOME BACK!";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -71,7 +71,7 @@
             this.buttonLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.buttonLogin.Font = new System.Drawing.Font("Arial Black", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonLogin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.buttonLogin.Location = new System.Drawing.Point(934, 615);
+            this.buttonLogin.Location = new System.Drawing.Point(684, 615);
             this.buttonLogin.Name = "buttonLogin";
             this.buttonLogin.Size = new System.Drawing.Size(644, 77);
             this.buttonLogin.TabIndex = 2;
@@ -84,7 +84,7 @@
             this.labelpassword.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labelpassword.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelpassword.ForeColor = System.Drawing.Color.Gray;
-            this.labelpassword.Location = new System.Drawing.Point(934, 469);
+            this.labelpassword.Location = new System.Drawing.Point(684, 469);
             this.labelpassword.Name = "labelpassword";
             this.labelpassword.Size = new System.Drawing.Size(644, 54);
             this.labelpassword.TabIndex = 3;
@@ -96,7 +96,7 @@
             this.labelemail.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labelemail.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelemail.ForeColor = System.Drawing.Color.Gray;
-            this.labelemail.Location = new System.Drawing.Point(934, 369);
+            this.labelemail.Location = new System.Drawing.Point(684, 369);
             this.labelemail.Name = "labelemail";
             this.labelemail.Size = new System.Drawing.Size(644, 54);
             this.labelemail.TabIndex = 4;
@@ -107,11 +107,11 @@
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(-6, 526);
+            this.label2.Location = new System.Drawing.Point(61, 414);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(989, 89);
+            this.label2.Size = new System.Drawing.Size(571, 89);
             this.label2.TabIndex = 5;
             this.label2.Text = "\"Percayalah pada diri sendiri. Ketahuilah bahwa ada sesuatu di dalam dirimu yang " +
     "lebih besar daripada rintangan apa pun.\"";
@@ -124,7 +124,7 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Arial Black", 50F, System.Drawing.FontStyle.Bold);
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(1072, 94);
+            this.label3.Location = new System.Drawing.Point(822, 94);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(413, 141);
             this.label3.TabIndex = 6;
@@ -135,7 +135,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1572, 761);
+            this.ClientSize = new System.Drawing.Size(1072, 761);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.labelemail);
